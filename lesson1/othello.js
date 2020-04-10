f33954c679334bb0516e3c42debc11e63c3c8a2f
duanhaonan <duanhaonan0113@163.com>
2020-04-10 09:15:23 +0800

    完成黑白棋基本游戏规则

--- lesson1/othello.js
+++ lesson1/othello.js
@@ -9,6 +9,17 @@ let board = [
     [0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
+let directions = [
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [1, -1],
+    [1, 0],
+    [-1, 0],
+    [-1, -1],
+    [-1, 1]
+]
+
 let container = document.getElementById('container')
 render()
 
@@ -31,34 +42,72 @@ function drawBoard () {
     }
 }
 
-function addClickEvent (element, x, y) {
-    element.addEventListener('click', (event) => {
-        /* console.log(x, y) */
-        if (!board[x][y]) {
-            board[x][y] = color
-        }
-        let ox = x
-        let oy = y
+function canMoveState (x, y, isCheck = false) {
+    if (board[x][y]) {
+        return false
+    }
+    let ox = x
+    let oy = y
+    let canMove = false
+    for (let direction of directions) {
+        let curDirection = false
         let hasOpposite = false
-        let canHandler = false
-        while(--y >= 0) {
-            if (board[x][y] ===  3 - color) {
+        x = ox
+        y = oy
+        while(true) {
+            x += direction[0]
+            y += direction[1]
+            if (x < 0 || x > 7 || y < 0 || y > 7) {
+                break
+            }
+            if (board[x][y] === 3 - color) {
                 hasOpposite = true
             }
-            if (board[x][y] === color && hasOpposite) {
-                canHandler = true
+            if (board[x][y] === color) {
+                if (hasOpposite) {
+                    curDirection = true
+                }
                 break
             }
-            if (!board[x][y]) {
+            if (board[x][y] === 0) {
                 break
             }
         }
-        if (canHandler) {
-            while(++y != oy) {
+        if (curDirection) {
+            canMove = true
+            while(true && !isCheck) {
+                x -= direction[0]
+                y -= direction[1]
                 board[x][y] = color
+                if (x === ox && y === oy) {
+                    break
+                }
             }
         }
+    }
+    if (canMove && !isCheck) {
         render()
         color = 3 - color
+    }
+    return canMove
+}
+
+function addClickEvent (element, x, y) {
+    element.addEventListener('click', (event) => {
+        canMoveState(x, y)
+        if (!checkChess()) {
+            color = 3 - color
+        }
     })
+}
+
+function checkChess () {
+    for (let x = 0;  x < 8; x++) {
+        for (let y = 0; y < 8; y++) {
+            if (canMoveState(x, y, true)) {
+                return true
+            }
+        }
+    }
+    return false
 }
